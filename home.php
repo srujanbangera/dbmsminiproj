abbd1c091af6ef03e817c2305a32587d3a6b0215
1by19is159 <1by19is159@bmsit.in>
2022-01-09 11:37:39 +0530

    first commit

---
+++ home.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    
+     figcaption {
+      color:black;
+      font-style: italic;
+      padding: 5px;
+      text-align: center;
+    }
+    * {
+      box-sizing: border-box;
+    }
+    
+    .column {
+      float: left;
+      width: 20%;
+      padding: 20px;
+    }
+    
+    /* Clearfix (clear floats) */
+    .row::after {
+      content: "";
+      clear: both;
+      display: table;
+    }
+    </style>
+<style>
+  
+   ul {
+     list-style-type: none;
+     margin: 0;
+     padding: 0;
+     overflow:hidden;
+     background-color: #FFD700;
+   }
+   
+   li {
+     float: left;
+   }
+   
+   li a {
+     display: block;
+     color: black;
+     text-align: center;
+     padding: 14px 16px;
+     font-size: large;
+     text-decoration: none;
+   }
+   
+   li a:hover {
+     background-color:ghostwhite;
+   }
+   .checked {
+  color: orange;
+}
+
+   </style>
+   
+  
+   </head>
+
+<body>
+   
+<ul>
+   <li><a class="active" href="home.html"><b>Home</b></a></li>
+   <li><a href="couriers.html"><b>Couriers</b></a></li>
+   <li><a href="staff.html"><b>Staff</b></a></li>
+   <li><a href="schedule.html"><b>Schedule</b></a></li>
+   <li><a href="delivery.html"><b>Delivery</b></a></li>
+ </ul>
+ <br>
+
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+ <div class="row">
+
+  <div class="column">
+    <style>
+      img {
+  border: 1px solid black;
+}
+body {
+    background-color:white;
+}
+    </style>
+    gotta type stg here
+    Welcome to CMS(Courier Management System)!
+  </div>
+  
+   </body>
+  
+</html>
